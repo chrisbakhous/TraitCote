51180b51251969f27eb043ac88c27e3d9a1daa78
chris <chris@DESKTOP-77OFPD7>
2017-08-18 02:57:26 +0200

    Radiales OK !!

---
+++ src/Util.java
@@ -0,0 +1,12 @@
+
+public class Util {
+	
+
+	
+	
+	public  static  Double distance (Point p1 ,Point p2) 
+	{		
+		return Math.sqrt(Math.pow(p2.getY()-p1.getY(),2)+Math.pow(p2.getX()-p1.getX(),2));
+	}	
+
+}
